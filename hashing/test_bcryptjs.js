010f5f0fe064d2f339ffa7e73e97efcd7fba4e2b
Nathan Krasney Laptop <natankrasney@gmail.com>
2019-12-16 13:54:28 +0200

    add time it took for round trip

--- hashing/test_bcryptjs.js
+++ hashing/test_bcryptjs.js
@@ -4,17 +4,21 @@ const clearTextPassword =
 process.argv[2] ? process.argv[2] : 'abc123';
 
 
+const start = new Date();
 const salt = bcrypt.genSaltSync(8);
 // --- there is also an async version of hashsync
-// --- hashed password should be stored in the databasexc
+// --- hashed password should be stored in the database
 const hashedPassword = bcrypt.hashSync(clearTextPassword, salt);
 
 console.log(`password : ${clearTextPassword} , hashed password : ${hashedPassword}`);
 
 // --- there is also an async version of compareSync
+// --- this is what should be done e.g. in login
 const passwordAreSame = bcrypt.compareSync(clearTextPassword,hashedPassword);
 
 console.log(` passwordAreSame : ${passwordAreSame}`);
+const end = new Date();
+console.log(`total time ms : ${end.getMilliseconds()-start.getMilliseconds()}`);
 
 
 
